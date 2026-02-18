fa43470f4b68a1c28f34bc1d0d9227b2eb907e47
mohsinraza2999 <mohsinraza2999@gmail.com>
2026-02-18 04:59:23 -0800

    add data in docker test stage

--- tests/test_api_routes.py
+++ tests/test_api_routes.py
@@ -1,5 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.inference.main import app
 
 test_app=TestClient(app)
